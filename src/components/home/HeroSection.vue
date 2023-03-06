012316fa356cc3cd7ac696fed545ea5c829852b7
devarogundade <devarogundade@gmail.com>
2023-03-06 19:16:43 +0100

    update contracts

--- src/components/home/HeroSection.vue
+++ src/components/home/HeroSection.vue
@@ -1,6 +1,10 @@
+
 <template>
     <main>
-        <div class="text"></div>
+        <div class="text">
+            <h3 class="btn-shine">Socializing DeFi on Fantom</h3>
+            <p>Darsh offers a peer to peer way of doing DeFi.</p>
+        </div>
     </main>
 </template>
 
@@ -21,5 +25,41 @@ main {
     background-image: url('../../assets/images/hero_gradient.png');
     background-size: cover;
     background-repeat: no-repeat;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+}
+
+.text h3 {
+    font-size: 68px;
+    font-weight: 700;
+}
+
+.text p {
+    font-size: 22px;
+    color: #9284BC;
+    margin-top: 24px;
+}
+
+.btn-shine {
+    background: linear-gradient(to right, var(--textnormal) 0, #b7aadd 10%, var(--textnormal) 20%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    animation: shine 5s infinite linear;
+    animation-fill-mode: forwards;
+    -webkit-text-size-adjust: none;
+    text-decoration: none;
+    white-space: nowrap;
+}
+
+@keyframes shine {
+    0% {
+        background-position: 0px;
+    }
+
+    100% {
+        background-position: 940px;
+    }
 }
 </style>
