5df25855ec26701b21a4c85b3f3c133318d2d464
devarogundade <devarogundade@gmail.com>
2023-03-07 21:07:53 +0100

    commit

--- src/scripts/Profile.js
+++ src/scripts/Profile.js
@@ -11,9 +11,12 @@ const Profile = {
                 address.length)
     },
     shortName: function (address, pad) {
-        // if (address.toLowerCase() == "0x36c8f83F1dcC316d591d5c15935951D240752558".toLowerCase()) {
-        //     return 'Ibro.ftm'
-        // }
+        if (address.toLowerCase() == "0x1a4d347fbdf09e2019b9a75d23d4be4ce74421ec".toLowerCase()) {
+            return 'Alice.ftm'
+        }
+        if (address.toLowerCase() == "0x25213fcda4eb079927cac2bcd5c513068b94babd".toLowerCase()) {
+            return 'Bob.ftm'
+        }
         let name = address.replace(/[^a-zA-Z]/gm,"")
         return '0x' + name.substring(1, pad)  + '.ftm'
     },
